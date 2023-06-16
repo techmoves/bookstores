bbc5de59f970565908928cdfe72cee6e5796e1b9
techmoves <muhndlovu28@gmail.com>
2023-06-16 23:57:22 +0200

    Fix bug

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import BooksItem from './BooksItem';
+import React, { useState } from "react";
+import BooksItem from "./BooksItem";
 
 export default function BookList() {
   const [books] = useState([
     {
       id: 1,
-      title: 'Romoe and Juliet',
-      author: 'William Shakespear',
+      title: "Romoe and Juliet",
+      author: "William Shakespear",
       completed: false,
     },
     {
       id: 1,
-      title: 'Art of hustling',
-      author: 'Dj sbu',
+      title: "Art of hustling",
+      author: "Dj sbu",
       completed: false,
     },
   ]);
