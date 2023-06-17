2191eedefde53fd30029705f5fe25849ada5d5f0
techmoves <muhndlovu28@gmail.com>
2023-06-17 11:59:06 +0200

    Fix bug

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import BooksItem from './BooksItem.js';
+import BooksItem from './BooksItem';
 
 export default function BookList() {
   const [books] = useState([
