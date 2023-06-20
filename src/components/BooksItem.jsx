48cf698bf1483876f2041db2ab8474c9086a0c01
techmoves <muhndlovu28@gmail.com>
2023-06-20 13:06:23 +0200

    Fiox linters

--- src/components/BooksItem.jsx
+++ src/components/BooksItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { getBooks, removeBook } from "../redux/books/booksSlice";
+import { getBooks, removeBook } from '../redux/books/booksSlice';
 
 export default function BooksItem({ list }) {
   const dispatch = useDispatch();
