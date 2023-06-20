5c41a8e431a6efbb910e3654e31a6e988807f264
techmoves <muhndlovu28@gmail.com>
2023-06-20 17:25:44 +0200

    Add adbook fuction

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
 
 // add book
 
 export default function Form() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [dispatch, useDispatch] = useState('');
+
+  const addSubmit = (e) => {
+    e.preventDefault();
+    const id = `item${Math.random()}`;
+    dispatch(addBook({ id, title, author }));
+    setTitle('');
+    setAuthor('');
+    useDispatch('');
+  };
   return (
     <div className="Form">
       <span className="add-newbook"> ADD NEW BOOK</span>
-      <form action="">
+      <form action="" onSubmit={addSubmit}>
         <input className="input-title" placeholder="Book title" type="text" />
-        <input
-          className="input-author"
-          placeholder="Book author"
-          type="text"
-        />
+        <input className="input-author" placeholder="Book author" type="text" />
         <button className="submit-btn" type="submit">
           ADD BOOK
         </button>
