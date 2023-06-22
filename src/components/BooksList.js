18012e1898b5b71bec114d001a4fc3f4e316685d
techmoves <muhndlovu28@gmail.com>
2023-06-22 12:03:58 +0200

    install axios refactors add & remove book

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,14 +1,27 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import BooksItem from './BooksItem';
+import { getBooks } from '../redux/books.js/booksSlice';
 
 export default function BookList() {
-  const books = useSelector((state) => state.book.bookItem);
+  const { booksItem, isLoading } = useSelector((state) => state.book.bookItem);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  });
+
+  const itemComponents = Object.keys(booksItem).flatMap((key) => BooksItem[key].map((book) => (
+    <BooksItem
+      key={`${key}-${book.title}`}
+      list={{ item_id: key, ...book }}
+    />
+  )));
 
   return (
     <>
-      {books.map((book) => (
-        <BooksItem key={book.id} list={book} />
-      ))}
+      <div className="Current-load"><h3>{isLoading === true ? 'Loading...' : '' }</h3></div>
+      {itemComponents}
     </>
   );
 }
